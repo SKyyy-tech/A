f8f1b4a366e327c5d4b2441f5b9edaf527a0a379
SKyy_tech <shresthaaakash208@gmail.com>
2025-05-04 18:59:05 -0700

    code added

--- Java.js
+++ Java.js
@@ -1,1 +1,7 @@
-console.log ("Hi")
+
+const hamburger = document.getElementById("hamburger")
+const navlinks = document.getElementById("navlinks")
+
+hamburger.addEventListener('click',() => {
+    navlinks.classList.toggle('active');
+});
