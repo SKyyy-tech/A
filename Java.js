c579faae259617d588c76932e378a6ed86b9d79a
SKyy_tech <shresthaaakash208@gmail.com>
2025-05-05 18:55:44 -0700

    File Added

--- Java.js
+++ Java.js
@@ -5,3 +5,18 @@ const navlinks = document.getElementById("navlinks")
 hamburger.addEventListener('click',() => {
     navlinks.classList.toggle('active');
 });
+
+
+
+const text = "Hello Amigos, I'm Steve";
+const typedText = document.getElementById('h-0');
+let index = 0;
+
+function type(){
+    if(index < text.length){
+       typedText.textContent += text.charAt(index);
+       index++;
+       setTimeout(type, 150);
+    }
+}
+type();
