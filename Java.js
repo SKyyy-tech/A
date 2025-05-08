708ec84e4a99298554dbb8d5bf52f08bb1b5acc8
SKyy_tech <shresthaaakash208@gmail.com>
2025-05-07 19:03:04 -0700

    file added

--- Java.js
+++ Java.js
@@ -20,3 +20,72 @@ function type(){
     }
 }
 type();
+
+
+
+const projects = [
+
+    {
+     image: "H.jpg",
+     title: "Animated navbar",
+     description: "A responsive navigation bar with smooth animation and uniques design"
+    },
+    {
+     image: "Cheese.jpg",
+     title: "HTML CSS",
+     description: "A responsive navigation bar with smooth animation and uniques design"
+    },
+    {
+     image: "Ham.jpg",
+     title: "Animated UI",
+     description: "A responsive navigation bar with smooth animation and uniques design"
+    }
+    
+]
+
+const container = document.getElementById("project-container");
+
+projects.forEach(project => {
+    const card = document.createElement("div");
+    card.className = "project-1";
+    card.innerHTML = `
+       <img src="${project.image}" alt="${project.title}" />
+       <h3>${project.title}</h3>
+       <p>${project.description}</p>
+    `;
+    container.appendChild(card);
+});
+
+
+const services = [
+
+    {
+       image : "Cucumber.jpg",
+       title : "HTML",
+       description: "HTML short for HyperText Markup Langauges"
+    },
+    {
+        image : "Nuke.jpg",
+        title : "CSS",
+        description :"CSS short for Cascading Style Sheet"
+    },
+    {
+        image : "Cheese.jpg",
+        title : "JS",
+        description : "JS short for Java Script"
+    }
+]
+
+
+const s_container = document.getElementById("Services_container");
+
+ services.forEach(service => {
+    const s_card = document.createElement("div");
+    s_card.className = "s-1";
+    s_card.innerHTMl =`
+        <img src="${service.image}" alt="${service.title}" />
+        <h3>${service.title}</h3>
+        <p>${service.description}</p>
+    `;
+    s_container.appendChild(s_card);
+ });
